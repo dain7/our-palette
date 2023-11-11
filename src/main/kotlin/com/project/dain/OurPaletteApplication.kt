975906789f7160d5ec64c8abc139b92e4d2b85f4
dain7 <dainnnnnn96@gmail.com>
2023-11-11 18:50:02 +0900

    first commit

---
+++ src/main/kotlin/com/project/dain/OurPaletteApplication.kt
@@ -0,0 +1,11 @@
+package com.project.dain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OurPaletteApplication
+
+fun main(args: Array<String>) {
+	runApplication<OurPaletteApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/dain/OurPaletteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.dain
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OurPaletteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
